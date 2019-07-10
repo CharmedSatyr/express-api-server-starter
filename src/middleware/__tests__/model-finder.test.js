863282c40aa23386eb9e6ccaf83ffd4cf4d73f02
Joseph <z@charmed.tech>
2019-07-10 13:40:45 -0700

    Update model-finder path and test arguments

--- src/middleware/__tests__/model-finder.test.js
+++ src/middleware/__tests__/model-finder.test.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const cwd = process.cwd();
-const modelFinder = require(`${cwd}/src/middleware/model-finder.js`);
+const modelFinder = require('../model-finder');
 
-const req = { params: { model: 'books' } };
+const req = { params: { model: 'book' } };
 const res = {};
 const next = jest.fn();
 
@@ -11,14 +10,16 @@ beforeAll(() => {
   modelFinder(req, res, next);
 });
 
-describe('model-finder.js', () => {
+describe('model-finder', () => {
   it('should append an object to the request as a `model` property', () => {
     expect(req.model).toBeDefined();
   });
+
   it('should call the `next` function', () => {
     modelFinder(req, res, next);
     expect(next).toHaveBeenCalled();
   });
+
   it('should call the `next` function if the model does not exist', () => {
     const badReq = { params: { mode: 'dummy' } };
     modelFinder(badReq, res, next);
