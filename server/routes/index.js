1af3694ef31d8feca03381c08a4ff7bc0509df13
Joseph <z@charmed.tech>
2019-07-10 17:34:12 -0700

    Temporarily remove auth middleware from routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,6 @@
 const router = require('express').Router();
 
 // Import middleware
-const auth = require('../auth/middleware');
 const modelFinder = require('../middleware/model-finder');
 
 // Import controllers
@@ -15,11 +14,11 @@ router.param('model', modelFinder);
 
 // Declare routes
 router.get('/', c.home);
-router.get(`/api/v1/:model`, c.getRecords);
-router.get(`/api/v1/:model/:id`, c.getRecords);
-router.post(`/api/v1/:model`, auth('create'), c.createRecord);
-router.put(`/api/v1/:model/:id`, auth('update'), c.updateRecord);
-router.patch(`/api/v1/:model/:id`, auth('update'), c.patchRecord);
-router.delete(`/api/v1/:model/:id`, auth('delete'), c.deleteRecord);
+router.get('/api/v1/:model', c.getRecords);
+router.get('/api/v1/:model/:id', c.getRecords);
+router.post('/api/v1/:model', c.createRecord);
+router.put('/api/v1/:model/:id', c.updateRecord);
+router.patch('/api/v1/:model/:id', c.patchRecord);
+router.delete('/api/v1/:model/:id', c.deleteRecord);
 
 module.exports = router;
