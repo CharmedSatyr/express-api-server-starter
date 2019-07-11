c6c4859a040f014273396732f2509cdf6238fe66
Joseph <z@charmed.tech>
2019-07-11 12:44:32 -0700

    Add routes tests

--- server/routes/__tests__/index.test.js
+++ server/routes/__tests__/index.test.js
@@ -9,24 +9,24 @@ const request = supergoose.mockRequest(server);
 beforeAll(supergoose.startDB);
 afterAll(supergoose.stopDB);
 
-describe('router', () => {
-  xit('should respond with a 404 on an invalid route', async () => {
+describe('v1 router', () => {
+  it('should respond with a 404 on an invalid route', async () => {
     const response = await request.get('/foo');
     expect(response.status).toBe(404);
   });
 
-  xit('responds with a 200 status to a GET for the root', async () => {
+  it('responds with a 200 status to a GET for the root', async () => {
     const response = await request.get('/');
     expect(response.status).toBe(200);
   });
 
-  xit('responds with a 200 status to a GET for /api/v1/book', async () => {
+  it('responds with a 200 status to a GET for /api/v1/book', async () => {
     const response = await request.get('/api/v1/book');
     expect(response.status).toBe(200);
   });
 
-  xit('responds with a 200 status to a GET for /api/v1/book/:id', async () => {
-    const book = {
+  it('responds with a 200 status to a GET for /api/v1/book/:id', async () => {
+    const obj = {
       title: word(),
       author: word(),
       isbn: alphaNumeric(),
@@ -34,54 +34,100 @@ describe('router', () => {
       description: word(),
       bookshelf: word(),
     };
-
-    const response = await request.post('/api/v1/book').send(book);
+    const response = await request.post('/api/v1/book').send(obj);
     const id = response.body._id;
     const results = await request.get(`/api/v1/book/${id}`);
+
     expect(results.status).toBe(200);
   });
 
-  xit('responds with a 200 status to a POST to /api/v1/book', async () => {
-    const obj = { name: 'demo', description: 'good', url: 'https://demo.com' };
+  it('responds with a 200 status to a POST to /api/v1/book', async () => {
+    const obj = {
+      title: word(),
+      author: word(),
+      isbn: alphaNumeric(),
+      image_url: word(),
+      description: word(),
+      bookshelf: word(),
+    };
     const response = await request.post('/api/v1/book').send(obj);
+
     expect(response.status).toBe(200);
     expect(response.body).toMatchObject(obj);
+
+    expect.assertions(2);
   });
 
-  xit('responds with a 200 status to a PUT to /api/v1/book/:id', async () => {
-    const obj = { description: 'good', name: 'demo', url: 'https://demo.com' };
+  it('responds with a 200 status to a PUT to /api/v1/book/:id', async () => {
+    const obj = {
+      title: word(),
+      author: word(),
+      isbn: alphaNumeric(),
+      image_url: word(),
+      description: word(),
+      bookshelf: word(),
+    };
     const response = await request.post('/api/v1/book').send(obj);
     const id = response.body._id;
     const update = {
-      description: 'good',
-      name: 'update',
-      url: 'https://demo.com',
+      title: word(),
+      author: word(),
+      isbn: alphaNumeric(),
+      image_url: word(),
+      description: word(),
+      bookshelf: word(),
     };
     const results = await request.put(`/api/v1/book/${id}`).send(update);
+
     expect(results.status).toBe(200);
     expect(results.body).toMatchObject(update);
+
+    expect.assertions(2);
   });
 
-  xit('responds with a 200 status to a PATCH to /api/v1/book/:id', async () => {
-    const obj = { name: 'demo', description: 'good', url: 'https://demo.com' };
+  it('responds with a 200 status to a PATCH to /api/v1/book/:id', async () => {
+    const obj = {
+      title: word(),
+      author: word(),
+      isbn: alphaNumeric(),
+      image_url: word(),
+      description: word(),
+      bookshelf: word(),
+    };
     const response = await request.post('/api/v1/book').send(obj);
     const id = response.body._id;
     const update = {
-      description: 'good',
-      name: 'update',
-      url: 'https://demo.com',
+      title: word(),
+      author: word(),
+      isbn: alphaNumeric(),
+      image_url: word(),
+      description: word(),
+      bookshelf: word(),
     };
     const result = await request.patch(`/api/v1/book/${id}`).send(update);
+
     expect(result.status).toBe(200);
     expect(result.body).toMatchObject(update);
+
+    expect.assertions(2);
   });
 
-  xit('responds with a 200 status to a DELETE to /api/v1/book/:id', async () => {
-    const obj = { name: 'demo', description: 'good', url: 'https://demo.com' };
+  it('responds with a 200 status to a DELETE to /api/v1/book/:id', async () => {
+    const obj = {
+      title: word(),
+      author: word(),
+      isbn: alphaNumeric(),
+      image_url: word(),
+      description: word(),
+      bookshelf: word(),
+    };
     const response = await request.post('/api/v1/book').send(obj);
     const id = response.body._id;
     const result = await request.delete(`/api/v1/book/${id}`);
+
     expect(result.status).toBe(200);
     expect(result.body).toMatchObject(obj);
+
+    expect.assertions(2);
   });
 });
