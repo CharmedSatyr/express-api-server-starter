3df52500263cac7be1641183968de9561558ffbd
Joseph <z@charmed.tech>
2019-07-11 15:23:57 -0700

    Update server import and add comments re Mongoose

--- index.js
+++ index.js
@@ -4,14 +4,17 @@
 require('dotenv').config();
 
 // Import the server's `start` method
-const { start } = require('./src/server');
+const { start } = require('./server');
 
 // Start up database server with options
 const mongoose = require('mongoose');
 
+// These options opt out of deprecations in the
+// MongoDB Node.js driver that Mongoose is phasing out
 const options = {
-  useNewUrlParser: true,
   useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
 };
 
 mongoose.connect(process.env.MONGODB_URI, options, err => {
