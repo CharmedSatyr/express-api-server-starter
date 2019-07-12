26b5c8ee1d1280de039449c3d750ba179ab28286
Joseph <z@charmed.tech>
2019-07-11 19:24:11 -0700

    Enable and clarify server/index.js export tests

--- server/__tests__/index.test.js
+++ server/__tests__/index.test.js
@@ -1,20 +1,34 @@
 'use strict';
 
-const { server /*, start */ } = require('../');
+const { app, start, stop } = require('../');
 
-const listen = jest.spyOn(server, 'listen'); // .mockImplementation(() => {});
+const listen = jest.spyOn(app, 'listen');
 const log = jest.spyOn(global.console, 'log').mockImplementation(() => {});
+const error = jest.spyOn(global.console, 'error').mockImplementation(() => {});
 
 const port = Math.floor(Math.random() * 65536).toString(); // Generate a random number >= 0 and < 65536
 
-// beforeAll(async () => start(port));
-
-describe('server `start` function', () => {
-  xit('should call `app.listen` with a given `port` and a callback', () => {
+describe('`start` function', () => {
+  it('should call `app.listen` with a given `port` and a callback', () => {
+    start(port);
     expect(listen).toHaveBeenCalledWith(port); //, expect.any(Function));
+    stop();
+
+    log.mockClear();
   });
 
-  xit('should log the `port` on which is it listening', () => {
+  it('should log the `port` on which is it listening', () => {
+    start(port);
     expect(log).toHaveBeenCalledWith(expect.stringContaining(port));
+    stop();
+
+    log.mockClear();
+  });
+
+  it('should log an error if `stop` is called while the server is not running', () => {
+    stop();
+    expect(error).toHaveBeenCalledWith(expect.any(String));
+
+    error.mockClear();
   });
 });
