a2c46fb7ab9e8c8389ec8571e2eb99f60f925311
Joseph <z@charmed.tech>
2019-07-14 09:59:40 -0700

    Add dummy auth routes, controllers, and views

---
+++ server/controllers/auth.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.login = (req, res) => {
+  res.status(200).render('login', { title: 'Login' });
+};
+
+exports.user = (req, res) => {
+  res.status(200).render('user', { title: 'User Profile' });
+};
