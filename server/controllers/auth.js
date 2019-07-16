cfa786c397ba13269205afb3d987e4ee7c361818
Joseph <z@charmed.tech>
2019-07-15 17:09:14 -0700

    Remove old /login route

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,9 +1,5 @@
 'use strict';
 
-exports.login = (req, res) => {
-  res.status(200).render('login', { title: 'Login' });
-};
-
 exports.user = (req, res) => {
   res.status(200).render('user', { title: 'User Profile', user: 'toad' });
 };
