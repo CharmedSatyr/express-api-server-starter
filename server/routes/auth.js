0a34c6a12fd239e73d65b33b2faaaf5774025a70
Joseph <z@charmed.tech>
2019-07-15 19:08:19 -0700

    Move cb controller from routes to controllers

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,15 +3,9 @@
 const router = require('express').Router();
 
 const c = require('../controllers/auth');
-const authorize = require('../controllers/mastodon');
 
 router.get('/login', c.login);
 router.get('/user', c.user);
-router.get('/callback', (req, res) => {
-  authorize(req).then(user => {
-    console.log('user:', user);
-    res.redirect('/user');
-  });
-}); // User is redirected here with code
+router.get('/callback', c.callback);
 
 module.exports = router;
