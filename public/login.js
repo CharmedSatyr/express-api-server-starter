f7736acf9d5e64b91af6fa50f0e603c9aa66745b
Joseph <z@charmed.tech>
2019-07-15 17:08:29 -0700

    Differentiate callback URL for testing

--- public/login.js
+++ public/login.js
@@ -4,7 +4,7 @@ const mastoURL = 'https://charmed.social/oauth/authorize';
 
 const options = {
   client_id: 'HY_q7kf5G1UtdMMq5kniprDZgwR4D9XvomIKz2vy5_o',
-  redirect_uri: 'http://127.0.0.1:3000/callback', // urn:ietf:wg:oauth:2.0:oob',
+  redirect_uri: 'http://127.0.0.1:3000/skallback', // urn:ietf:wg:oauth:2.0:oob',
   response_type: 'code',
   scope: 'read:accounts',
 };
