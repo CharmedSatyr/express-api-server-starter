044de7e7d6a17b2e28a400a0fa435f92937cfabe
Joseph <z@charmed.tech>
2019-07-17 20:13:17 -0700

    Update auth0 callback; add login links

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,12 +9,14 @@ const a0 = require('../controllers/auth/auth0');
 // User Profile
 router.get('/user', a.user);
 
-// Login with local OAuth2 methods
+// Auth with local OAuth2 methods
 router.get('/login', o.login);
 router.get('/callback', o.callback);
+router.get('/logout', o.logout);
 
 // Login with Auth0
 router.get('/login-auth0', a0.login);
-router.get('/auth0-cb', a0.callback);
+router.get('/callback-auth0', a0.callback);
+router.get('/logout-auth0', a0.logout);
 
 module.exports = router;
