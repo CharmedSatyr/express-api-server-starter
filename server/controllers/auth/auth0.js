f8b3c51de2f7bf0e6269f5bde98f90c5c9494845
Joseph <z@charmed.tech>
2019-07-17 20:14:21 -0700

    Add auth0 logout controller

--- server/controllers/auth/auth0.js
+++ server/controllers/auth/auth0.js
@@ -2,6 +2,7 @@
 
 const passport = require('passport');
 
+// Login
 exports.login = (req, res) => {
   passport.authenticate('auth0', {
     failureRedirect: '/',
@@ -12,6 +13,7 @@ exports.login = (req, res) => {
     };
 };
 
+// Callback
 exports.callback = (req, res, next) => {
   passport.authenticate('auth0', (err, user /* , info */) => {
     if (err) {
@@ -35,4 +37,31 @@ exports.callback = (req, res, next) => {
       res.status(200).redirect('/user');
     });
   })(req, res, next);
+};
+
+// Log Out
+exports.logout = (req, res) => {
+  req.logout();
+
+  let returnTo = req.protocol + '://' + req.hostname;
+  const port = req.connection.localPort;
+
+  if (port !== undefined && port !== 80 && port !== 443) {
+    returnTo += ':' + port;
+  }
+
+  let logoutURL = `https://${process.env.AUTH0_DOMAIN}/v2/logout`;
+
+  const options = {
+    client_id: process.env.AUTH0_CLIENT_ID,
+    returnTo,
+  };
+
+  const queryString = Object.keys(options)
+    .map(key => key + '=' + encodeURIComponent(options[key]))
+    .join('&');
+
+  logoutURL += '?' + queryString;
+
+  res.redirect(logoutURL);
 };
